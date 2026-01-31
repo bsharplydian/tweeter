3cf0c1e56cfbe9db1f33d71e4070c83d42e572c1
Jacob <jmemmott13@gmail.com>
2026-01-31 15:08:11 -0700

    refactor: simplified UserInfoHooks

--- tweeter-web/src/components/userInfo/UserInfoHooks.ts
+++ tweeter-web/src/components/userInfo/UserInfoHooks.ts
@@ -2,31 +2,8 @@ import { useContext } from "react";
 import { UserInfoContext, UserInfoActionsContext } from "./UserInfoContexts";
 import { AuthToken, User } from "tweeter-shared";
 
-interface UserInfoActions {
-  updateUserInfo: (
-    currentUser: User,
-    displayedUser: User | null,
-    authToken: AuthToken,
-    remember: boolean,
-  ) => void;
-  clearUserInfo: () => void;
-  setDisplayedUser: (user: User) => void;
-}
-
-export const useUserInfoActions = (): UserInfoActions => {
-  const { updateUserInfo, clearUserInfo, setDisplayedUser } = useContext(
-    UserInfoActionsContext,
-  );
-  return {
-    updateUserInfo: (
-      currentUser: User,
-      displayedUser: User | null,
-      authToken: AuthToken,
-      remember: boolean,
-    ) => updateUserInfo(currentUser, displayedUser, authToken, remember),
-    clearUserInfo: clearUserInfo,
-    setDisplayedUser: setDisplayedUser,
-  };
+export const useUserInfoActions = () => {
+  return useContext(UserInfoActionsContext);
 };
 
 export const useUserInfo = () => {
