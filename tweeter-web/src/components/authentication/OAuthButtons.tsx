8bb149d1586bc97bc37367769c497d03855752a1
Jacob <jmemmott13@gmail.com>
2026-01-30 21:36:53 -0700

    fix: removed ToastActionsContext from OAuthButtons.tsx

--- tweeter-web/src/components/authentication/OAuthButtons.tsx
+++ tweeter-web/src/components/authentication/OAuthButtons.tsx
@@ -3,19 +3,14 @@ import { ToastActionsContext } from "../toaster/ToastContexts";
 import { ToastType } from "../toaster/Toast";
 import { OverlayTrigger, Tooltip } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useMessageActions } from "../toaster/MessageHooks";
 
 const OAuthButtons = () => {
-  const { displayToast } = useContext(ToastActionsContext);
+  const { displayInfoMessage } = useMessageActions();
   const displayInfoMessageWithDarkBackground = (message: string): void => {
     //
 
-    displayToast(
-      ToastType.Info,
-      message,
-      3000,
-      undefined,
-      "text-white bg-primary",
-    );
+    displayInfoMessage(message, 3000, "text-white bg-primary");
   };
 
   return (
